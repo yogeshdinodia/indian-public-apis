d5311ab8ed57e38d516ace6512754e95e2d4169e
Yogesh Dinodia <ydinodia@gmail.com>
2023-03-19 19:28:30 +0530

    Add Readme and fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # indian-public-apis
 Free Open Indian public APIs 
+
+# How To Generate API Token
+1. Go to Softhuge Technologies Website and create user account
+    https://softhuge.com/login?next=user/
+
+2. Navigate to "API Token" from sidebar menu.
+3. Click "Create Token" Button
+4. Save the API Token Code and Click "Generate"
+5. Replace "API Token" getting from step 4 into file "apiCall.php"
--- apiCall.php
+++ apiCall.php
@@ -1,13 +1,14 @@
 <?php
-$ApiToken="YogeshDinodia";
+$ApiToken="SOF-br21c-y38zr5uuxcgk-r0qoxwdl57q3";	// Add Your Api Token From softhuge.com, check readme for know how to generate token
 $ApiUrl="https://softhuge.com/api/v1";
 if(isset($_REQUEST['ifsc'])){
 	$ifsc = trim(strip_tags($_REQUEST["ifsc"]));
 	$ifsc_length = strlen($ifsc);
-	if($ifsc=='' || $ifsc_length<9){
+	if($ifsc_length<9){
 		$msg_array["StatusCode"]=400;
 		$msg_array["Message"]="Invalid IFSC Code";
 		array_push($json, $msg_array);
+		echo json_encode($json[0]);
 	}
 	else{
 		$curl = curl_init();
